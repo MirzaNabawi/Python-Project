1552b59541ab5b701376eb85baef457435f52dd8
MizCodes <119789438+MirzaNabawi@users.noreply.github.com>
2024-08-12 16:12:36 +0600

    Add files via upload

---
+++ DistanceCalculator.py
@@ -0,0 +1,8 @@
+#code
+import math
+def distance (x , y):
+    return math.sqrt(abs(x)**2 + abs(y)**2)
+x =float (input("Enter the X Coordinate: "))
+y =float (input("Enter the Y Coordinate: "))
+
+print ("The Distance is:", distance(x, y)) 
